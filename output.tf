5df7be1b66f972513dde48b2540bc1901c7f06df
Arnaud Lheureux <arnaudlh@users.noreply.github.com>
2019-10-11 18:06:00 +0800

    Documentation update

--- output.tf
+++ output.tf
@@ -1,11 +1,14 @@
 output "id" {
+  description = "Output the object ID"
   value = azurerm_automation_account.auto_account.id
 }
 
 output "name" {
+  description = "Output the object name"
   value = azurerm_automation_account.auto_account.name
 }
 
 output "object" {
+  description = "Output the full object"
   value = azurerm_automation_account.auto_account
 }
